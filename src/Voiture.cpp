725f7cc12d0fcd9aaee2f851659ffe63d47d1ce8
p2002495 <mateomunoz@orange.fr>
2022-09-23 14:11:06 +0200

    réindentation de Voiture

--- src/Voiture.cpp
+++ src/Voiture.cpp
@@ -3,99 +3,112 @@
 #include <iostream>
 
 // Constructeur de la classe Voiture
-Voiture::Voiture() {
+Voiture::Voiture()
+{
 }
 
 // Destructeur de la classe Voiture
-Voiture::~Voiture() {
+Voiture::~Voiture()
+{
 }
 
 // Déplace la voiture vers la droite
-void Voiture::MoveRight() {
+void Voiture::MoveRight()
+{
     position.x += vitesse;
 }
 
 // Déplace la voiture vers la gauche
-void Voiture::MoveLeft() {
+void Voiture::MoveLeft()
+{
     position.x -= vitesse;
 }
 
 // Déplace la voiture vers le haut
-void Voiture::MoveUp() {
+void Voiture::MoveUp()
+{
     position.y += vitesse;
 }
 
 // Déplace la voiture vers le bas
-void Voiture::MoveDown() {
+void Voiture::MoveDown()
+{
     position.y -= vitesse;
 }
 
 // Renvoie la position (x;y) de la voiture
-Vec2 Voiture::get_position() {
+Vec2 Voiture::get_position()
+{
     return position;
 }
 
 // Modifie la position (x;y) de la voiture
-void Voiture::set_position(Vec2 new_pos) {
+void Voiture::set_position(Vec2 new_pos)
+{
     position = new_pos;
 }
 
 // Modifie la position de la cible (parking / place / sortie)
-void Voiture::setTargetPosition(Vec2 new_pos) {
+void Voiture::setTargetPosition(Vec2 new_pos)
+{
     TargetPosition = new_pos;
 }
-
 
 // Déplace la voiture vers la position de la cible (parking / place / sortie)
 // On changer la position de la cible quand la voiture entre dans le parking
 // Nouvelle position cible = position place de parking etc.
-bool Voiture::MoveToTargetPosition() {
+bool Voiture::MoveToTargetPosition()
+{
 
     // Si la voiture est au dessus du parking
-    if (position.y > TargetPosition.y) {
+    if (position.y > TargetPosition.y)
+    {
 
         MoveDown();
-        if (position.y < TargetPosition.y) {
+        if (position.y < TargetPosition.y)
+        {
 
             position.y = TargetPosition.y;
         }
     }
     // Si la voiture est au dessous du parking
-    else if (position.y < TargetPosition.y) {
+    else if (position.y < TargetPosition.y)
+    {
 
         MoveUp();
-        if (position.y > TargetPosition.y) {
+        if (position.y > TargetPosition.y)
+        {
 
             position.y = TargetPosition.y;
-
         }
     }
     // Si la voiture est à gauche du parking
-    if (position.x < TargetPosition.x) {
+    if (position.x < TargetPosition.x)
+    {
 
         MoveRight();
-        if (position.x > TargetPosition.x) {
+        if (position.x > TargetPosition.x)
+        {
 
             position.x = TargetPosition.x;
-
         }
-    } 
-    // Si la voiture est à droite du parking 
-    else if (position.x > TargetPosition.x) {
+    }
+    // Si la voiture est à droite du parking
+    else if (position.x > TargetPosition.x)
+    {
 
         MoveLeft();
-        if (position.x < TargetPosition.x) {
+        if (position.x < TargetPosition.x)
+        {
 
             position.x = TargetPosition.x;
-
         }
     }
     // Si la voiture est à la position cible
-    if (position.x == TargetPosition.x && position.y == TargetPosition.y) {
+    if (position.x == TargetPosition.x && position.y == TargetPosition.y)
+    {
 
         return true;
-
     }
     return false;
 }
-
